f886648c48b1472d87617dd995f2269776987c72
DESKTOP-6RMTQEJ\Serega <riabov.s@yandex.ru>
2019-02-17 22:11:53 +0300

    OtherResourcesScreen update

--- src/main/app/pages/home/screens/other-resources-screen/OtherResourcesScreen.js
+++ src/main/app/pages/home/screens/other-resources-screen/OtherResourcesScreen.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import '../../home.scss';
+import './other-resources-screen.scss';
 import {Title} from '../../../../components/title/Title';
 
 export class OtherResourcesScreen extends React.Component {
   render() {
     return (
-      <div className="home__content">
+      <div className="other-resources-screen">
         <Title>Ищите нас на других ресурсах:</Title>
-        <div></div>
+        <div className="other-resources-screen__container">
+          <div className="other-resources-screen__item">
+            <span>Instagram<div className="other-resources-screen__item-underline"></div></span>
+          </div>
+          <div className="other-resources-screen__item">
+            <span>Наш Блог<div className="other-resources-screen__item-underline"></div></span>
+          </div>
+          <div className="other-resources-screen__item">
+            <span>Vkontakte<div className="other-resources-screen__item-underline"></div></span>
+          </div>
+          <div className="other-resources-screen__item">
+            <span>Facebook<div className="other-resources-screen__item-underline"></div></span>
+          </div>
+          <div className="other-resources-screen__item">
+            <span>Media<div className="other-resources-screen__item-underline"></div></span>
+          </div>
+        </div>
       </div>
     );
   }
