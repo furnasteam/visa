2c79e714a85be97a473351b82790981e1ccef6e8
DESKTOP-6RMTQEJ\Serega <riabov.s@yandex.ru>
2019-02-18 23:29:01 +0300

    add arrow-green

--- src/main/app/pages/home/screens/other-resources-screen/OtherResourcesScreen.js
+++ src/main/app/pages/home/screens/other-resources-screen/OtherResourcesScreen.js
@@ -8,20 +8,22 @@ export class OtherResourcesScreen extends React.Component {
     return (
       <div className="other-resources-screen">
         <Title>Ищите нас на других ресурсах:</Title>
+        <div className="other-resources-screen__arrow-green-icon">
+        </div>
         <div className="other-resources-screen__container">
-          <div className="other-resources-screen__item">
+          <div className="other-resources-screen__item other-resources-screen__resources-col3">
             <span>Instagram<div className="other-resources-screen__item-underline"></div></span>
           </div>
-          <div className="other-resources-screen__item">
+          <div className="other-resources-screen__item other-resources-screen__resources-col3">
             <span>Наш Блог<div className="other-resources-screen__item-underline"></div></span>
           </div>
-          <div className="other-resources-screen__item">
+          <div className="other-resources-screen__item other-resources-screen__resources-col3">
             <span>Vkontakte<div className="other-resources-screen__item-underline"></div></span>
           </div>
-          <div className="other-resources-screen__item">
+          <div className="other-resources-screen__item other-resources-screen__resources-col2">
             <span>Facebook<div className="other-resources-screen__item-underline"></div></span>
           </div>
-          <div className="other-resources-screen__item">
+          <div className="other-resources-screen__item other-resources-screen__resources-col2">
             <span>Media<div className="other-resources-screen__item-underline"></div></span>
           </div>
         </div>
